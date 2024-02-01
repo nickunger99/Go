1c7218565dd8e3e6944498c94696cdc01f59c5a7
nickunger99 <nicolas.unger@yahoo.com.br>
2024-02-01 15:12:48 -0300

    trabalhando com variaveis

--- 2.Alura/1.Go/hello/hello.go
+++ 2.Alura/1.Go/hello/hello.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Olá mundo, Nicolas!")
+	nome := "Nicolas"
+	versao := 1.21
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O endereço da minha variavel comando é", &comando)
+	fmt.Println("O comando escolhido foi", comando)
 }
