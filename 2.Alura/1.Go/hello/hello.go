a4ac94f08c29fbf4fa2dce6958efd5b871552868
nickunger99 <nicolas.unger@yahoo.com.br>
2024-02-05 10:10:55 -0300

    Requisições em GO e laço infinito

--- 2.Alura/1.Go/hello/hello.go
+++ 2.Alura/1.Go/hello/hello.go
@@ -2,38 +2,37 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
-
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
-
-	// if comando == 1 {
-	// 	fmt.Println("Monitorando..")
-	// } else if comando == 2 {
-	// 	fmt.Println("Exibindo Logs")
-	// } else if comando == 0 {
-	// 	fmt.Println("Saindo do programa")
-	// } else {
-	// 	fmt.Println("Não conheço este comando")
-	// }
-
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando..")
-	case 2:
-		fmt.Println("Exibindo Logs")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+	for {
+		exibeMenu()
+		comando := leComando()
+		// if comando == 1 {
+		// 	fmt.Println("Monitorando..")
+		// } else if comando == 2 {
+		// 	fmt.Println("Exibindo Logs")
+		// } else if comando == 0 {
+		// 	fmt.Println("Saindo do programa")
+		// } else {
+		// 	fmt.Println("Não conheço este comando")
+		// }
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
-
 }
 
 func exibeIntroducao() {
@@ -54,4 +53,18 @@ func leComando() int {
 	fmt.Scan(&comandoLido)
 	fmt.Println("O comando escolhido foi", comandoLido)
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando..")
+	site := "https://www.alura.com.br"
+	resp, _ := http.Get(site)
+	// fmt.Println(resp)
+	// site := "https://httpbin.org/status/404" // ou 200
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
