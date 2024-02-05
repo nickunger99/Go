224d7611bdac919d014535056ac50907ead70b36
nickunger99 <nicolas.unger@yahoo.com.br>
2024-02-05 15:56:51 -0300

    first commit - Go OOP

---
+++ 2.Alura/2.Go_OOP/banco/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Nicolas"
+	var numeroAgencia int = 589
+	var numeroConta int = 123456
+	var saldo float64 = 2232.22
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+}
