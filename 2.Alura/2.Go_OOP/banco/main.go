9f02a2ccbaef182056774dbfa79c214885a3a3bb
nickunger99 <nicolas.unger@yahoo.com.br>
2024-02-06 10:26:45 -0300

    referencia ponteiro e metodo

--- 2.Alura/2.Go_OOP/banco/main.go
+++ 2.Alura/2.Go_OOP/banco/main.go
@@ -9,10 +9,45 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
 func main() {
-	contaDoNicolas := ContaCorrente{"Nicolas",
-		589, 223456, 23232.22}
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
 
-	fmt.Println(contaDoNicolas)
+	fmt.Println(contaDaSilvia)
 
+	fmt.Println(contaDaSilvia.sacar(200))
+
+	fmt.Println(contaDaSilvia)
+
+	// contaDoNicolas := ContaCorrente{"Nicolas",
+	// 	589, 223456, 23232.22}
+
+	// fmt.Println(contaDoNicolas)
+
+	// var contaDaCris *ContaCorrente
+	// contaDaCris = new(ContaCorrente)
+	// contaDaCris.titular = "Cris"
+	// contaDaCris.saldo = 500
+
+	// var contaDaCris2 *ContaCorrente
+	// contaDaCris2 = new(ContaCorrente)
+	// contaDaCris2.titular = "Cris"
+	// contaDaCris2.saldo = 500
+
+	// fmt.Println(*contaDaCris)
+	// fmt.Println(&contaDaCris)
+	// fmt.Println(&contaDaCris2)
+	// fmt.Println(*contaDaCris == *contaDaCris2)
+	// fmt.Println(contaDaCris == contaDaCris2)
 }
