d05856e3bc2df7dd51e6ba0e08a38850e6e4df9b
nickunger99 <nicolas.unger@yahoo.com.br>
2024-02-06 11:09:29 -0300

    retornos, pacotes e visibilidade

--- 2.Alura/2.Go_OOP/banco/main.go
+++ 2.Alura/2.Go_OOP/banco/main.go
@@ -1,34 +1,32 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
+import (
+	"Go/2.Alura/2.Go_OOP/banco/contas"
+	"fmt"
+)
 
 func main() {
-	contaDaSilvia := ContaCorrente{}
-	contaDaSilvia.titular = "Silvia"
-	contaDaSilvia.saldo = 500
+	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
+	contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 100}
 
+	status := contaDaSilvia.Transferir(200, &contaDoGustavo)
+
+	fmt.Println(status)
 	fmt.Println(contaDaSilvia)
+	fmt.Println(contaDoGustavo)
 
-	fmt.Println(contaDaSilvia.sacar(200))
+	// contaDaSilvia := ContaCorrente{}
+	// contaDaSilvia.titular = "Silvia"
+	// contaDaSilvia.saldo = 500
 
-	fmt.Println(contaDaSilvia)
+	// fmt.Println(contaDaSilvia)
+
+	// fmt.Println(contaDaSilvia.sacar(200))
+
+	// fmt.Println(contaDaSilvia)
+
+	// status, valor := contaDaSilvia.depositar(2000)
+	// fmt.Println(status, valor)
 
 	// contaDoNicolas := ContaCorrente{"Nicolas",
 	// 	589, 223456, 23232.22}
